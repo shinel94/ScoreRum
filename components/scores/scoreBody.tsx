e9a0d44058f7df48484ebc03fc06b6b0d08219ff
Yeony.kim <yeony.kim@machbase.com>
2023-01-12 15:37:15 +0900

    add tour event when first come and no see tour

--- components/scores/scoreBody.tsx
+++ components/scores/scoreBody.tsx
@@ -1,4 +1,4 @@
-import Joyride from "react-joyride";
+import Joyride, { CallBackProps, STATUS } from "react-joyride";
 import styles from "./scoreBody.module.scss";
 import controllerStyles from "./scoreController.module.scss";
 import Vex from "vexflow";
@@ -23,6 +23,7 @@ export default function ScoreBody(props: scoreBodyType) {
     {
       target: `.${styles.scoreBody}`,
       content: "Drummer를 위한 악보 툴 Score rum에 오신 것을 환영합니다.",
+      disableBeacon: true,
     },
     {
       target: `.${styles.scoreBody}`,
@@ -31,13 +32,11 @@ export default function ScoreBody(props: scoreBodyType) {
     },
     {
       target: `.${controllerStyles.scoreController}`,
-      content:
-        "마우스를 통해서 이 패널을 제어해서 악보를 그릴 수 있습니다.",
+      content: "마우스를 통해서 이 패널을 제어해서 악보를 그릴 수 있습니다.",
     },
     {
       target: `.${controllerStyles.length}`,
-      content:
-        "이것을 통해서 음표의 길이를 제어할 수 있습니다.",
+      content: "이것을 통해서 음표의 길이를 제어할 수 있습니다.",
     },
     {
       target: `.${controllerStyles.rest}`,
@@ -46,29 +45,24 @@ export default function ScoreBody(props: scoreBodyType) {
     },
     {
       target: `.${controllerStyles.soundList}`,
-      content:
-        "이 패널을 통해서 드럼 악기의 음표를 그릴 수 있습니다.",
+      content: "이 패널을 통해서 드럼 악기의 음표를 그릴 수 있습니다.",
     },
     {
       target: `.${controllerStyles.soundList}`,
-      content:
-        "다음과 같이 약어로 악기를 표현합니다.",
+      content: "다음과 같이 약어로 악기를 표현합니다.",
     },
     {
       target: `.${controllerStyles.soundList}`,
-      content:
-        "HH: high hat / CR: crash / RI: ride / SN: snare",
+      content: "HH: high hat / CR: crash / RI: ride / SN: snare",
     },
     {
       target: `.${controllerStyles.soundList}`,
-      content:
-        "HT: high top / MT: middle top / FT: floor top / BA: base drum",
+      content: "HT: high top / MT: middle top / FT: floor top / BA: base drum",
     },
-    
+
     {
       target: `.${controllerStyles.soundList}`,
-      content:
-        "각각 악기는 토글 형태로, 선택됨과 선택되지 않음으로 나뉩니다.",
+      content: "각각 악기는 토글 형태로, 선택됨과 선택되지 않음으로 나뉩니다.",
     },
     {
       target: `.${controllerStyles.soundActions}`,
@@ -77,33 +71,27 @@ export default function ScoreBody(props: scoreBodyType) {
     },
     {
       target: `.${controllerStyles.soundActions}`,
-      content:
-        "DEL 버튼을 통해서 마지막에 추가된 음표를 제거합니다.",
+      content: "DEL 버튼을 통해서 마지막에 추가된 음표를 제거합니다.",
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "제어 패널을 사용하지 않고 키보드로도 악보를 그릴 수 있습니다.",
+      content: "제어 패널을 사용하지 않고 키보드로도 악보를 그릴 수 있습니다.",
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "음표 길이 제어는 Q(늘리기)와 E(줄이기)로 할 수 있습니다.",
+      content: "음표 길이 제어는 Q(늘리기)와 E(줄이기)로 할 수 있습니다.",
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "쉼표는 R 로 선택 및 해제할 수 있습니다.",
+      content: "쉼표는 R 로 선택 및 해제할 수 있습니다.",
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "그리고 악기의 경우, 패널의 첫번째 줄은 순서대로 ASDF",
+      content: "그리고 악기의 경우, 패널의 첫번째 줄은 순서대로 ASDF",
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "패널의 두번째 줄은 순서대로 ZXCV 를 나타냅니다.",
+      content: "패널의 두번째 줄은 순서대로 ZXCV 를 나타냅니다.",
     },
     {
       target: `.${styles.scoreBody}`,
@@ -117,8 +105,7 @@ export default function ScoreBody(props: scoreBodyType) {
     },
     {
       target: `.${styles.scoreBody}`,
-      content:
-        "그럼 이제 여러분의 악보를 그려보세요",
+      content: "그럼 이제 여러분의 악보를 그려보세요",
     },
   ];
 
@@ -166,8 +153,19 @@ export default function ScoreBody(props: scoreBodyType) {
     updateScoreContent(props.score.scoreId, props.score.toContent());
   };
 
+  const handleJoyrideCallback = (data: CallBackProps) => {
+    const { status } = data;
+    const finishedStatuses: string[] = [STATUS.FINISHED];
+
+    if (finishedStatuses.includes(status)) {
+      window.localStorage.setItem("TOUR-DONE", "true");
+    }
+  };
+
   useEffect(() => {
-    setRunTour(true);
+    if (!window.localStorage.getItem("TOUR-DONE")) {
+      setRunTour(true);
+    }
   }, []);
 
   useEffect(() => {
@@ -281,6 +279,7 @@ export default function ScoreBody(props: scoreBodyType) {
     <div className={styles.score}>
       <Joyride
         steps={steps}
+        callback={handleJoyrideCallback}
         continuous
         hideCloseButton
         showProgress
